9427dec10c7f9b82c3c41ee2e24f0c70506c4e49
Vito Farenga <vitofarenga@libero.it>
2022-09-02 16:25:48 +0200

    prima milestone completata

---
+++ movie.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/index.php';
+
+class movie {
+    private $title;
+    private $genre;
+    private $language;
+
+    function __construct($_title, $_genre, $_language){
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->language = $_language;
+
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function getGenre(){
+        return $this->genre;
+    }
+
+    public function getLanguage(){
+        return $this->language;
+    }
+}
